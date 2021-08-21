fe3bbce387b6c99be7cde7dcb53e85d493c78440
RoUchiha <swaggsikh@gmail.com>
2021-08-21 00:44:27 -0500

    finished Search class. basic structure for Scraper

--- lib/search.rb
+++ lib/search.rb
@@ -4,20 +4,22 @@ require 'addressable/uri'
 
 class Search
 
-    attr_accessor :location :zip_url
+    attr_accessor :location, :zip_url
 
     def initialize(location)
-        
-        searcher(location)
+        if location < 00501 || location > 99950
+            raise ArgumentError
+        else
+            searcher(location)
+        end
     end
 
     def searcher(zip)
         uri = Addressable::URI.parse('https://www.google.com/search?q=+rain+chances')
         p = uri.query_values
         uri.query_values = p.merge('loc' => zip)
-        zip_url = uri.to_s
-        
+        @zip_url = uri.to_s
     end
-
-
 end
+
+Search.new(75007).zip_url
