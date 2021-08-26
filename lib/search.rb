0042be2bbf0231f935bc9050ea4d30499992c711
RoUchiha <swaggsikh@gmail.com>
2021-08-25 19:21:16 -0500

    fixed all classes. porgram working as intended now

--- lib/search.rb
+++ lib/search.rb
@@ -7,7 +7,7 @@ class Search
     attr_accessor :location, :zip_url
 
     def initialize(location)
-        if location < 00501 || location > 99950
+        if location < 501 || location > 99950
             raise ArgumentError 
         else
             searcher(location)
@@ -15,7 +15,17 @@ class Search
     end
 
     def searcher(zip)
-        uri = Addressable::URI.parse('https://weather.com/weather/today/l/' + zip.to_s)        
+        fixed_zip = zip_convert(zip)
+        uri = Addressable::URI.parse('https://weather.com/weather/today/l/' + fixed_zip)        
         @zip_url = uri.to_s
     end
+
+    def zip_convert(code)
+        zcode = code.to_s
+        while zcode.length < 5 do
+            zcode = "0" + zcode
+        end
+        return zcode
+    end
+
 end
